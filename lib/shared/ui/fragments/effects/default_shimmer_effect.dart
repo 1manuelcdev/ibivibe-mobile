1e6c599c5ee19ccd14840097a2e286d4157500f5
Manuel C. <manuelwn21@gmail.com>
2026-03-04 15:02:11 -0300

    feat(defaultShimmerEffect): standartizes default effect for skeletons

---
+++ lib/shared/ui/fragments/effects/default_shimmer_effect.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:forui/forui.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
+ShimmerEffect customShimmerEffect(BuildContext context) {
+  return ShimmerEffect(
+    begin: .centerStart,
+    baseColor: context.theme.colors.muted,
+    highlightColor: context.theme.colors.muted.withAlpha(90),
+    duration: Duration(milliseconds: 2500),
+  );
+}
