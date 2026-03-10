98ed16e269e04caebc0c783f3a7a1adaef2cfd20
Manuel C. <manuelwn21@gmail.com>
2026-03-10 11:45:12 -0300

    feat(cache): Implement clearAllData method for cache management and enhance logging

--- lib/core/cache/cache_database_service.dart
+++ lib/core/cache/cache_database_service.dart
@@ -1,3 +1,4 @@
+import 'package:ibiapabaapp/core/logger/logger.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sembast/sembast_io.dart';
@@ -10,5 +11,27 @@ class CacheDatabaseService {
     await appDir.create(recursive: true);
     final dbPath = join(appDir.path, 'ibiapabaapp_cache.db');
     db = await databaseFactoryIo.openDatabase(dbPath);
+  }
+
+  static Future<void> clearAllData() async {
+    try {
+      final stores = [
+        intMapStoreFactory.store('cities_store'),
+        stringMapStoreFactory.store('cities_metadata_store'),
+      ];
+
+      await db.transaction((txn) async {
+        for (final store in stores) {
+          await store.delete(txn);
+        }
+      });
+
+      logger.i('🧹 [Sembast] Cache local aniquilado com sucesso.');
+    } catch (e, stack) {
+      logger.e('❌ [Sembast] Erro ao limpar cache: $e');
+      logger.e(stack);
+
+      rethrow;
+    }
   }
 }
