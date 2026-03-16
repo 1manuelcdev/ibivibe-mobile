d1d2fe89f9a8441850c848a830acf5bb08ea1ea0
Manuel C. <manuelwn21@gmail.com>
2026-03-16 19:27:39 -0300

    refactor(MediasController): rewrites logging stategy to use LogHandler

--- lib/features/medias/presentation/controllers/medias_controller.dart
+++ lib/features/medias/presentation/controllers/medias_controller.dart
@@ -1,14 +1,23 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:ibiapabaapp/core/logger/handlers/controller_log_handler.dart';
+import 'package:ibiapabaapp/core/logger/log_tags.dart';
+import 'package:ibiapabaapp/core/logger/logger.dart';
 import 'package:ibiapabaapp/features/auth/presentation/providers/session_provider.dart';
 import 'package:ibiapabaapp/features/medias/domain/entity/media.dart';
 import 'package:ibiapabaapp/features/medias/presentation/providers/medias_providers.dart';
+import 'package:logger/logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'medias_controller.g.dart';
 
-// TODO: implementar ControllerLogHandler
 @riverpod
-class EntityMedias extends _$EntityMedias {
+class EntityMedias extends _$EntityMedias with ControllerLogHandler {
+  @override
+  Logger get logger => ref.read(loggerProvider);
+
+  @override
+  LogFeature get feature => LogFeature.medias;
+
   @override
   Future<List<Media>> build(EntityType entityType, String entityId) async {
     final session = ref.watch(sessionProvider.select((s) => s));
@@ -28,8 +37,17 @@ class EntityMedias extends _$EntityMedias {
     );
 
     return result.fold(
-      (failure) => throw Exception(failure.message),
-      (media) => media,
+      (failure) {
+        logControllerError(
+          action: MediaAction.getEntityMedia,
+          failure: failure,
+        );
+        throw Exception(failure.message);
+      },
+      (media) {
+        logControllerSuccess(action: MediaAction.getEntityMedia);
+        return media;
+      },
     );
   }
 
